c705f4e6b2603314cf021a134641992512cba9b2
saleh2001k <saleh.ayman2001@gmail.com>
2023-08-21 13:30:35 +0300

    RUSTy v0.1

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Footer.scss"
+
+function Footer() {
+  return <footer>&copy; 2018</footer>;
+}
+export default Footer;
---
+++ src/Components/Form/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import "./Form.scss";
+
+const Form = ({ handleApiCall }) => {
+  const formRef = React.createRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      method: "GET",
+      url: "https://pokeapi.co/api/v2/pokemon",
+    };
+    handleApiCall(formData);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} ref={formRef}>
+        <label>
+          <span>URL: </span>
+          <input name="url" type="text" />
+          <button type="submit">GO!</button>
+        </label>
+        <label className="methods">
+          <span id="get">GET</span>
+          <span id="post">POST</span>
+          <span id="put">PUT</span>
+          <span id="delete">DELETE</span>
+        </label>
+      </form>
+    </>
+  );
+};
+
+export default Form;
